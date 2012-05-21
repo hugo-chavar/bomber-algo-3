fdb1126fdfc21575bcc29dc6b5a52c99c7d8e0ff
Hugo Chavar <hechavar@gmail.com>
2012-05-20 21:30:20 -0300

    Diagrama de clases Se agregan las clases e interfaces

---
+++ testDotNet/testDotNet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Bomberman!");
+            //modifica copia de la copia
+            Console.WriteLine("modifica copia de la copia");
+            Console.WriteLine("Escriba su mensaje y presione Enter para salir..");
+            Console.ReadLine();
+        }
+    }
+}
