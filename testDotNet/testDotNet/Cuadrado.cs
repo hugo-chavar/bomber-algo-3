ba508c82c351e6f98aed8388cc4d01f08b4ccf6d
Hugo Chavar <hechavar@gmail.com>
2012-05-25 18:00:55 -0300

    modifico el cuadrado aksdf

--- testDotNet/testDotNet/Cuadrado.cs
+++ testDotNet/testDotNet/Cuadrado.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("**********");
             Console.WriteLine("*        *");
-            Console.WriteLine("*        *");
+
             Console.WriteLine("*        *");
             Console.WriteLine("*        *");
             Console.WriteLine("**********");
