f02e0fc11b74cbebdea8715c22f9688a9ce34a17
Ezequiel <ezemr_cuervo@hotmail.com>
2012-05-25 16:48:30 -0300

    Aca va un intento de comiteo, me mate con la clase que hice

---
+++ testDotNet/testDotNet/Rana.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Rana
+    {
+        public void croar(){
+            Console.WriteLine("Estoy croando");
+        }
+    }}
