af130d1df44f4a2705eb163f7b0a7ad9af0c5882
Hugo Chavar <hechavar@gmail.com>
2012-06-01 10:47:03 -0300

    Agrego armamento, corrijo errores de proyecto

---
+++ Source/Bomberman/Armamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Armamento
+    {
+        protected int OndaExpansiva;
+        protected Punto Posicion;
+    }
+}
