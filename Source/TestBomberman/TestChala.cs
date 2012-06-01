f22fa20b92267bd16aef49a94270642e0c6e07e2
Mart?n Carrera <martinc.92@hotmail.com>
2012-06-01 15:03:19 -0300

    Agrego Test Chala, con un test, y las clases para que funcione Modifique tambien la clase Personaje.

---
+++ Source/TestBomberman/TestChala.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Bomberman.Personaje;
+using Bomberman.Articulo;
+
+namespace TestBomberman
+{   [TestFixture]
+    class TestChala
+    {
+        [Test]
+        public void TestComerChalaDuplicaVelocidadDelComedor()
+        {
+            int velocidad;
+            Bombita unBombita = new Bombita();
+            Chala unArticulo = new Chala();
+            velocidad = unBombita.Velocidad;
+
+            unBombita.comer(unArticulo);
+
+            Assert.AreEqual(2*velocidad, unBombita.Velocidad);
+
+         }
+    }
+}
