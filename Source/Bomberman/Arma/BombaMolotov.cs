6d1cab244fd14123d76e47981685cd4d54512b08
Hugo Chavar <hechavar@gmail.com>
2012-06-01 15:47:33 -0300

    Hago que las clases que tienen que implementar Posicionable, lo hagan.. Ver la notacion, los nombres de los atributos ponerlos siempre en minuscula la primer letra usar camelCase.

--- Source/Bomberman/Arma/BombaMolotov.cs
+++ Source/Bomberman/Arma/BombaMolotov.cs
@@ -10,12 +10,12 @@ namespace Bomberman.Arma
         
         public override void inicializarBomba(int x, int y)
         {                     
-                this.Retardo = 1;
-                this.PoderDeDestruccion = 5;
-                this.OndaExpansiva = 3;
+                this.retardo = 1;
+                this.poderDeDestruccion = 5;
+                this.ondaExpansiva = 3;
                 this.exploto = false;
                 Punto unaPosicion = new Punto(x,y);
-                Posicion = unaPosicion;
+                posicion = unaPosicion;
 
                 
                      
@@ -33,11 +33,11 @@ namespace Bomberman.Arma
 
 
 
-            unPuntoAux.Y = Posicion.Y;
-            for (i = -(OndaExpansiva); i < OndaExpansiva+1; i++){
+            unPuntoAux.Y = posicion.Y;
+            for (i = -(ondaExpansiva); i < ondaExpansiva+1; i++){
             
                 
-             unPuntoAux.X = i + Posicion.X;
+             unPuntoAux.X = i + posicion.X;
             listaDevolucion[j] = unPuntoAux;
                 j = j + 1;
         }
@@ -47,16 +47,6 @@ namespace Bomberman.Arma
 
             return listaDevolucion;
     }
-       
- 
-
-
-
-
-
-
-
-            
 
 }
-    }
+}
