a1a3a6ee334a49ce8f25212c4f9ba08f2ca27ac2
Hugo Chavar <hechavar@gmail.com>
2012-06-01 10:47:03 -0300

    Agrego armamento, corrijo errores de proyecto

---
+++ Source/Bomberman/Bomba.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public abstract class Bomba: Armamento
+    {
+
+        protected int PoderDeDestruccion;
+        protected int Retardo;
+        protected bool exploto;
+        
+
+
+
+
+        public virtual void inicializarBomba(int x, int y) {
+        }
+        
+        public int getRetardo()
+        { return this.Retardo; }
+        
+        public void cuandoPasaElTiempo(int t)
+        {
+            if (t >= Retardo)
+            {
+
+                this.explotar();
+            }
+        }
+
+        public void explotar()
+        {
+            this.exploto = true; // true = EXPLOTADO , false = ACTIVADA Y NO EXPLOTADA, //
+
+
+        }
+
+        public bool estaExplotada()
+        {
+            return this.exploto;
+        }
+
+        public int getOndaExpansiva()
+        {
+            return OndaExpansiva;
+        }
+
+        public void modificarRetardo(int porcentaje)
+        {
+            this.Retardo = Retardo * porcentaje / 100;
+        }
+
+
+
+       
+}
+
+}
---
+++ Source/Bomberman/BombaMolotov.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class BombaMolotov: Bomba
+    {
+        
+        public override void inicializarBomba(int x, int y)
+        {                     
+                this.Retardo = 1;
+                this.PoderDeDestruccion = 5;
+                this.OndaExpansiva = 3;
+                this.exploto = false;
+                Punto unaPosicion = new Punto(x,y);
+                Posicion = unaPosicion;
+
+                
+                     
+        }
+
+
+        public Punto[] calcularCasillerosExplotados (){
+            int i;
+            int j = 1;
+            Punto[] listaDevolucion = new Punto[15];
+      
+
+
+            Punto unPuntoAux = new Punto(1,1);
+
+
+
+            unPuntoAux.Y = Posicion.Y;
+            for (i = -(OndaExpansiva); i < OndaExpansiva+1; i++){
+            
+                
+             unPuntoAux.X = i + Posicion.X;
+            listaDevolucion[j] = unPuntoAux;
+                j = j + 1;
+        }
+            
+
+            
+
+            return listaDevolucion;
+    }
+       
+ 
+
+
+
+
+
+
+
+            
+
+}
+    }
