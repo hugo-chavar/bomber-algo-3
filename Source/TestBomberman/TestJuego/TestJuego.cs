0243cf68f24b60f10e1fbb14a9e336b9c83bca76
Hugo Chavar <hechavar@gmail.com>
2012-06-02 12:01:48 -0300

    Primer Test de clase Juego

--- Source/TestBomberman/TestJuego/TestJuego.cs
+++ Source/TestBomberman/TestJuego/TestJuego.cs
@@ -7,5 +7,20 @@ namespace TestBomberman.TestJuego
     [TestFixture]
     public class TestJuego
     {
+        private Juego unJuego;
+
+        [TestFixtureSetUp]
+        public void TestSetup()
+        {
+            this.unJuego = Juego.Instancia();
+        }
+        
+        [Test]
+        public void DosReferenciasAJuedoDebenSerElMismoObjeto()
+        {
+            //verifico que funcione el Singleton
+            Juego otroJuego = Juego.Instancia();
+            Assert.AreSame(otroJuego, this.unJuego);
+        }
     }
 }
