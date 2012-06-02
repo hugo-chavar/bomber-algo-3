65d7866fa129b68788e179ba91f76c1979d6b2c4
Hugo Chavar <hechavar@gmail.com>
2012-06-02 12:16:41 -0300

    arreglo test de juego que no funciona

--- Source/TestBomberman/TestJuego/TestJuego.cs
+++ Source/TestBomberman/TestJuego/TestJuego.cs
@@ -14,7 +14,6 @@ namespace TestBomberman.TestJuego
         {
             this.unJuego = Juego.Instancia();
         }
-        
         [Test]
         public void DosReferenciasAJuedoDebenSerElMismoObjeto()
         {
