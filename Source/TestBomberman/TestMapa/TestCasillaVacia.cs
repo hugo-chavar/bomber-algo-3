86b063f16bf9bf938430f6758e075674712de4a4
Hugo Chavar <hechavar@gmail.com>
2012-06-02 23:12:53 -0300

    Mas tests Empiezo a testear las casillas, y corrijo algunas cositas, Andy revisa el TestBloqueAcero

---
+++ Source/TestBomberman/TestMapa/TestCasillaVacia.cs
@@ -0,0 +1,29 @@
+﻿using NUnit;
+using NUnit.Framework;
+using Bomberman;
+using Bomberman.Mapa;
+using Bomberman.Mapa.Casilla;
+using Bomberman.Personaje;
+
+namespace TestBomberman.TestMapa
+{
+    [TestFixture]
+    class TestCasillaVacia
+    {
+        private CasillaVacia unaCasillaVacia;
+        
+        [TestFixtureSetUp]
+        public void TestSetup()
+        {
+            Punto unaPosicion = new Punto(2, 3);
+            this.unaCasillaVacia = new CasillaVacia(unaPosicion);
+        }
+
+        [Test]
+        public void esTransitablePorUnPersonaje()
+        {
+            Personaje unPersonaje = new Bombita();
+            Assert.IsTrue(this.unaCasillaVacia.TransitablePor(unPersonaje));
+        }
+    }
+}
