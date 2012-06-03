6c10a1c671aa561a5fe43507c63fb023c1d5df3a
Hugo Chavar <hechavar@gmail.com>
2012-06-02 23:58:54 -0300

    Agrego pruebas, hay una que toque de los bloques que est? fallando.

--- Source/Bomberman/Articulo/Articulo.cs
+++ Source/Bomberman/Articulo/Articulo.cs
@@ -7,9 +7,21 @@ using Bomberman.Personaje;
 namespace Bomberman.Articulo
 {
     public abstract class Articulo : IComible
-    {
+    {   
+        protected bool estaOculto;
+
+        public Articulo()
+        {
+            estaOculto = true;
+        }
 
         public abstract void ModificarComedor(IComedor comedor);
+
+        public bool EstaOculto
+        {
+            set { this.estaOculto = EstaOculto; }
+            get { return this.estaOculto; }
+        }
 
     }
 }
