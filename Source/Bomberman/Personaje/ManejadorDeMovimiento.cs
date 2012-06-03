b28ca54cd2e0f327f5e82d9d86c6b207dcec97bc
Ezequiel <ezemr_cuervo@hotmail.com>
2012-06-03 20:07:53 -0300

    Agrego manejador de movimiento , tengo que probarlo todavia todavia, pasa que necesito a mapa y casillero, ahor veo cmo hago para ver si se puede probar

---
+++ Source/Bomberman/Personaje/ManejadorDeMovimiento.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bomberman.Personaje
+{
+    class ManejadorDeMovimiento
+    {
+        private Personaje personajeInterno;
+
+        // Definir ENUM: 1: arriba, 2 : abajo, 3 : Derecha, 4 : Izquierda //
+
+        public ManejadorDeMovimiento(Personaje personaje)
+        {
+            this.personajeInterno = personaje;
+        }
+
+        public Punto MoverPersonaje(int MOV){
+            Punto posicionDestino = this.ConseguirDestino(personajeInterno.Posicion, MOV);
+                if (!(posicionDestino.EsPuntoValido()))
+                {
+                  return personajeInterno.Posicion;
+                }
+            Bomberman.Mapa.Casilla.Casilla casilleroDestino = Juego.Juego.Instancia().Ambiente.obtenerCasilla(posicionDestino);
+            Bomberman.Mapa.Casilla.Casilla casilleroOrigen =  Juego.Juego.Instancia().Ambiente.obtenerCasilla(personajeInterno.Posicion);
+             if (casilleroDestino.PermiteTransitarUn(personajeInterno))
+            {
+                // casilleroOrigen.Destransitar(personajeInterno) falta Destransitar //
+                personajeInterno.Posicion = posicionDestino;
+                casilleroDestino.Transitar(personajeInterno);
+            }
+                return posicionDestino;
+            }
+        
+
+        public Punto ConseguirDestino(Punto Posicion, int MOV)
+        {
+            switch (MOV)
+            {
+                case 0:                  // Arriba //
+                    {
+                        Posicion.AumentarYEn(1);
+                        break;
+                    }
+                case 1:             // Abajo //
+                    {
+                        Posicion.AumentarYEn(-1);
+                        break;
+                    }
+                case 2:             // Derecha //
+                    {
+                        Posicion.AumentarXEn(1);
+                        break;
+                    }
+                case 3:             // Izquierda //
+                    {
+                        Posicion.AumentarXEn(-1);
+                        break;
+                    }
+            }
+            return Posicion;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+
+ 
