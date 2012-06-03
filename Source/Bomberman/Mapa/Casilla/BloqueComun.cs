e00c52234184255788aedf29cc482eaec41ca5ab
Ezequiel <ezemr_cuervo@hotmail.com>
2012-06-03 13:53:35 -0300

    Agrego constantes resistenciaLadrillo, resistenciaCemento en bloqueComun

--- Source/Bomberman/Mapa/Casilla/BloqueComun.cs
+++ Source/Bomberman/Mapa/Casilla/BloqueComun.cs
@@ -4,19 +4,23 @@ namespace Bomberman.Mapa.Casilla
 {
     public class BloqueComun:Obstaculo
     {
+
+        private const resistenciaLadrillos = 5;
+        private const resistenciaCemento = 10;
+
         private BloqueComun(Punto Posicion, int UnidadesResistencia)
             : base(UnidadesResistencia) // Saque la posicion de ak
         { } //: base(Posicion, UnidadesResistencia) // Nota: los obstaculos ya no necesitan posicion, son un State de Casilla
 
         public static BloqueComun CrearBloqueCemento(Punto posicion)
         { 
-            BloqueComun BloqueCemento = new BloqueComun(posicion, 10); // Meter constante para la "vida" del bloque
+            BloqueComun BloqueCemento = new BloqueComun(posicion, resistenciaCemento); 
             return (BloqueCemento);
         }
 
         public static BloqueComun CrearBloqueLadrillos(Punto posicion)
         {
-            BloqueComun BloqueLadrillos = new BloqueComun(posicion, 5); // Meter constante para la "vida" del bloque
+            BloqueComun BloqueLadrillos = new BloqueComun(posicion, resistenciaLadrillos); 
             return (BloqueLadrillos);
         }
 
