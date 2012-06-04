0b05de457ff889c6785a9e598dc7bcaf076d1325
Ezequiel <ezemr_cuervo@hotmail.com>
2012-06-03 23:10:32 -0300

    Agrego una prueba, creo que puede haber conflicto con el cambio de Hugo, ya mismo les aviso

---
+++ Source/TestBomberman/TestPersonaje/TestPersonajeII.cs
@@ -0,0 +1,46 @@
+﻿using NUnit;
+using NUnit.Framework;
+using Bomberman.Mapa.Casilla;
+using Bomberman;
+using Bomberman.Personaje;
+using System.Collections.Generic;
+using Bomberman.Mapa;
+namespace TestBomberman.TestPersonaje
+{
+    [TestFixture]
+    class TestPersonajeII
+    {
+        private FabricaDeCasillas unaFabricaDeCasillas;
+        private Punto posicionOrigen;
+        private Casilla origen;
+        private Punto posicionDestino;
+        private Casilla destino;
+        private Personaje unPersonaje;
+        private Mapa unMapa;
+        private ManejadorDeMovimiento manejador;
+
+        [TestFixtureSetUp]
+        public void TestSetup()
+        {
+            this.unaFabricaDeCasillas = new FabricaDeCasillas();
+            this.posicionOrigen = new Punto(1, 1);
+            this.origen = unaFabricaDeCasillas.FabricarPasillo(posicionOrigen);
+            this.posicionDestino = new Punto(2, 1);
+            this.destino = unaFabricaDeCasillas.FabricarPasillo(posicionDestino);
+            this.unPersonaje = new Bombita(posicionOrigen);
+            this.unMapa = new Mapa(3, 3);
+            this.unMapa.agregarCasilla(origen);
+            this.unMapa.agregarCasilla(destino);
+            this.manejador = new ManejadorDeMovimiento(unPersonaje, unMapa);
+                        
+        }
+
+        [Test]
+        public void CuandoManejadorMueveAPersonajeUnoYLoConsigue()
+        {
+            manejador.MoverPersonaje(2);
+            Assert.AreEqual(unPersonaje.Posicion.X, posicionDestino.X);
+        }
+    }
+
+}
