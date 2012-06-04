663913430d2846e5e80d315f7cdd80763c226e19
Hugo Chavar <hechavar@gmail.com>
2012-06-04 12:03:53 -0300

    8 pruebas mas... vamos por 59 (1 en rojo)

--- Source/TestBomberman/TestMapa/TestMapa.cs
+++ Source/TestBomberman/TestMapa/TestMapa.cs
@@ -14,18 +14,55 @@ namespace TestBomberman.TestMapa
         private const int ANCHOMAPA = 5;
         private const int ALTOMAPA = 5;
         private Mapa unMapa;
+        private Mapa otroMapa;
         private Casilla unaCasilla;
         private Punto pos;
         private IMovible movil;
 
+        [TestFixtureSetUp]
+        public void TestSetup()
+        {
+            //creo un mapa 5x5 con esta distribucion (P = Pasillo, * = BloqueAcero):
+            //      P P P P P
+            //      P * P * P
+            //      P P P P P
+            //      P * P * P
+            //      P P P P P
+
+            Punto unaPosicion;
+            Casilla unaCasilla;
+            this.unMapa = new Mapa(ANCHOMAPA, ANCHOMAPA);
+            this.otroMapa = new Mapa(ANCHOMAPA, ANCHOMAPA);
+            int i, j;
+            for (i = 0; i < ANCHOMAPA; i++)
+                for (j = 0; j < ANCHOMAPA; j++)
+                {
+                    unaPosicion = new Punto(i, j);
+                    if ((i & 1) == 0 && (j & 1) == 0)
+                    {
+                        //ambos son numeros pares
+                        unaCasilla = FabricaDeCasillas.FabricarCasillaConBloqueAcero(unaPosicion);
+                    }
+                    else
+                    {
+                        //uno de los dos es impar
+                        unaCasilla = FabricaDeCasillas.FabricarPasillo(unaPosicion);
+                    }
+                    this.unMapa.agregarCasilla(unaCasilla);
+                }
+
+        }
+
+
+        
         [Test]
         public void AgregarUnaCasillaNulaLanzaNoExisteCasillaException()
         {
-            unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
+            //unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
             Casilla otraCasilla = null;
             try
             {
-                unMapa.agregarCasilla(otraCasilla);
+                otroMapa.agregarCasilla(otraCasilla);
                 Assert.Fail();
             }
             catch (NoExisteCasillaException)
@@ -40,12 +77,12 @@ namespace TestBomberman.TestMapa
         [Test]
         public void AgregarUnaCasillaConPosicionNulaLanzaPosicionNulaException()
         {
-            unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
+            //unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
             pos = null;
             Casilla otraCasilla = new Casilla(pos);
             try
             {
-                unMapa.agregarCasilla(otraCasilla);
+                otroMapa.agregarCasilla(otraCasilla);
                 Assert.Fail();
             }
             catch (PosicionNulaException)
@@ -60,12 +97,12 @@ namespace TestBomberman.TestMapa
         [Test]
         public void AgregarUnaCasillaConPosicionFueraDeRangoLanzaPuntoFueraDeRangoEnMapaException()
         {
-            unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
-            pos = new Punto(7, 2);
+            //unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
+            pos = new Punto(ANCHOMAPA + 1, ALTOMAPA - 1);
             Casilla otraCasilla = new Casilla(pos);
             try
             {
-                unMapa.agregarCasilla(otraCasilla);
+                otroMapa.agregarCasilla(otraCasilla);
                 Assert.Fail();
             }
             catch (PuntoFueraDeRangoEnMapaException)
@@ -80,98 +117,124 @@ namespace TestBomberman.TestMapa
         [Test]
         public void UnMapaCreadoTieneElAnchoIndicadoEnElConstructor()
         { 
-            unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
+            //unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
             Assert.AreEqual(unMapa.DimensionHorizontal, ANCHOMAPA);
         }
 
         [Test]
         public void UnMapaCreadoTieneElAltoIndicadoEnElConstructor()
         {
-            unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
+            //Mapa otroMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
             Assert.AreEqual(unMapa.DimensionVertical, ALTOMAPA);
         }
 
         [Test]
         public void NuevoMapaContieneCasillaAgregada()
         {
-            Mapa otroMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
-            pos = new Punto(4, 2);
-            Casilla otraCasilla = FabricaDeCasillas.FabricarCasillaConBloqueCemento(pos);//new Casilla(pos);//
+            //Mapa otroMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
+            Punto unaPos = new Punto(4, 2);
+            Casilla otraCasilla = FabricaDeCasillas.FabricarCasillaConBloqueCemento(unaPos);//new Casilla(pos);//
             otroMapa.agregarCasilla(otraCasilla);
-            Assert.IsTrue(otroMapa.ExisteCasillaEnPosicion(pos));
+            Assert.IsTrue(otroMapa.ExisteCasillaEnPosicion(unaPos));
         }
 
         [Test]
         public void NuevoMapaNoContieneCasillas()
         {
-            unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
+            //Mapa otroMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
             Punto unaPos = new Punto(0, 0);
-            Assert.IsFalse(unMapa.ExisteCasillaEnPosicion(unaPos));
+            Assert.IsFalse(otroMapa.ExisteCasillaEnPosicion(unaPos));
             
         }
 
         [Test]
         public void AgregarCasillaDejaLaCasillaEnLaPosicionCorrecta()
         {
-            this.pos = new Punto(2, 3);
-            this.unMapa = new Mapa(ANCHOMAPA, ALTOMAPA);
-            this.unaCasilla = FabricaDeCasillas.FabricarPasillo(pos);
-            this.unMapa.agregarCasilla(unaCasilla);
-            Assert.AreSame(unMapa.ObtenerCasilla(pos) , unaCasilla);
+            Punto unaPos = new Punto(2, 2);
+            Casilla unaCasilla = FabricaDeCasillas.FabricarPasillo(unaPos);
+            otroMapa.agregarCasilla(unaCasilla);
+            Assert.AreSame(otroMapa.ObtenerCasilla(unaPos), unaCasilla);
         }
-        
-        [TestFixtureSetUp]
-        public void TestSetup()
-        {
-            //creo un mapa 5x5 con esta distribucion (P = Pasillo, * = BloqueAcero):
-             //      P P P P P
-             //      P * P * P
-             //      P P P P P
-             //      P * P * P
-             //      P P P P P
-            
-            Punto unaPosicion;
-            Casilla unaCasilla;
-            this.unMapa = new Mapa(5,5);
-            int i,j;
-            for ( i = 0 ; i < 5 ; i++)
-                for ( j = 0; j < 5; j++)
-                {
-                    unaPosicion = new Punto(i, j);
-                    if ((i & 1) == 0 && (j & 1) == 0)
-                    {
-                        //ambos son numeros pares
-                        unaCasilla = FabricaDeCasillas.FabricarCasillaConBloqueAcero(unaPosicion);
-                    }
-                    else
-                    { 
-                        //uno de los dos es impar
-                        unaCasilla = FabricaDeCasillas.FabricarPasillo(unaPosicion);
-                    }
-                    this.unMapa.agregarCasilla(unaCasilla);
-                }
 
+
+
+        [Test]
+        public void ExisteCasillaEnPosicionDevuelveTrueSiPreguntoPorLaPosDeArribaYEsaPosicionExisteEnMapa()
+        {
+            //La posicion existe porque en fixtureSetup unMapa es creado de 5x5 con casillas en todas sus posiciones [0..4][0..4]
+            this.pos = new Punto(0, 0);
+            //Casilla unaCasilla = FabricaDeCasillas.FabricarPasillo(this.pos);
+            //this.unMapa.agregarCasilla(unaCasilla);
+            //Assert.IsTrue(this.unMapa.ExisteCasillaEnPosicion(this.pos));
+            Punto posSup = this.pos.PosicionSuperior();
+            Assert.IsTrue(unMapa.ExisteCasillaEnPosicion(posSup));
         }
 
         [Test]
-        public void ExisteCasillaEnPosicionDevuelveTrueSiVoyAArribaYEsaPosicionExisteEnMapa()
+        public void ExisteCasillaEnPosicionDevuelveTrueSiPreguntoPorLaPosDeAbajoYEsaPosicionExisteEnMapa()
         {
-            this.pos = new Punto(0, 0);
-            Casilla unaCasilla = FabricaDeCasillas.FabricarPasillo(this.pos);
-            this.unMapa.agregarCasilla(unaCasilla);
-            Assert.IsTrue(this.unMapa.ExisteCasillaEnPosicion(this.pos));
-            //Punto otra = this.pos.PosicionSuperior();
-            //Assert.IsTrue(unMapa.ExisteCasillaEnPosicion(otra));
+            //La posicion existe porque en fixtureSetup unMapa es creado de 5x5 con casillas en todas sus posiciones [0..4][0..4]
+            this.pos = new Punto(0, 1);
+            Punto posInf = this.pos.PosicionInferior();
+            Assert.IsTrue(unMapa.ExisteCasillaEnPosicion(posInf));
         }
 
-        /*[Test]
-        public void ExisteCasillaEnPosicionDevuelveTrueSiVoyAAbajoYEsaPosicionExisteEnMapa()
+        [Test]
+        public void ExisteCasillaEnPosicionDevuelveTrueSiPreguntoPorLaPosDeLaDerechaYEsaPosicionExisteEnMapa()
         {
+            //La posicion existe porque en fixtureSetup unMapa es creado de 5x5 con casillas en todas sus posiciones [0..4][0..4]
             this.pos = new Punto(0, 1);
-            Punto otra = this.pos.PosicionInferior();
-            Assert.IsTrue(unMapa.ExisteCasillaEnPosicion(otra));
+            Punto posDer = this.pos.PosicionDerecha();
+            Assert.IsTrue(unMapa.ExisteCasillaEnPosicion(posDer));
         }
-        */
+
+        [Test]
+        public void ExisteCasillaEnPosicionDevuelveTrueSiPreguntoPorLaPosDeLaIzquierdaYEsaPosicionExisteEnMapa()
+        {
+            //La posicion existe porque en fixtureSetup unMapa es creado de 5x5 con casillas en todas sus posiciones [0..4][0..4]
+            this.pos = new Punto(1, 1);
+            Punto posIzq = this.pos.PosicionIzquierda();
+            Assert.IsTrue(unMapa.ExisteCasillaEnPosicion(posIzq));
+        }
+
+        [Test]
+        public void ExisteCasillaEnPosicionDevuelveFalseSiPreguntoPorLaPosDeArribaYEsaPosicionNoExisteEnMapa()
+        {
+            //La posicion existe porque en fixtureSetup unMapa es creado de 5x5 con casillas en todas sus posiciones [0..4][0..4]
+            this.pos = new Punto(4, 4);
+            //Casilla unaCasilla = FabricaDeCasillas.FabricarPasillo(this.pos);
+            //this.unMapa.agregarCasilla(unaCasilla);
+            //Assert.IsTrue(this.unMapa.ExisteCasillaEnPosicion(this.pos));
+            Punto posSup = this.pos.PosicionSuperior();
+            Assert.IsFalse(unMapa.ExisteCasillaEnPosicion(posSup));
+        }
+
+        [Test]
+        public void ExisteCasillaEnPosicionDevuelveFalseSiPreguntoPorLaPosDeAbajoYEsaPosicionExisteEnMapa()
+        {
+            //La posicion existe porque en fixtureSetup unMapa es creado de 5x5 con casillas en todas sus posiciones [0..4][0..4]
+            this.pos = new Punto(3, 0);
+            Punto posInf = this.pos.PosicionInferior();
+            Assert.IsFalse(unMapa.ExisteCasillaEnPosicion(posInf));
+        }
+
+        [Test]
+        public void ExisteCasillaEnPosicionDevuelveFalseSiPreguntoPorLaPosDeLaDerechaYEsaPosicionNoExisteEnMapa()
+        {
+            //La posicion existe porque en fixtureSetup unMapa es creado de 5x5 con casillas en todas sus posiciones [0..4][0..4]
+            this.pos = new Punto(4, 2);
+            Punto posDer = this.pos.PosicionDerecha();
+            Assert.IsFalse(unMapa.ExisteCasillaEnPosicion(posDer));
+        }
+
+        [Test]
+        public void ExisteCasillaEnPosicionDevuelveFalseSiPreguntoPorLaPosDeLaIzquierdaYEsaPosicionNoExisteEnMapa()
+        {
+            //La posicion existe porque en fixtureSetup unMapa es creado de 5x5 con casillas en todas sus posiciones [0..4][0..4]
+            this.pos = new Punto(0, 3);
+            Punto posIzq = this.pos.PosicionIzquierda();
+            Assert.IsFalse(unMapa.ExisteCasillaEnPosicion(posIzq));
+        }
         /*[Test]
         public void PosicionDeArribaTienePasilloBombitaPuedeMoverseHaciaArriba()
         {
