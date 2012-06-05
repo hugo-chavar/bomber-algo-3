a925c60a79c898701e6c6fed7c0e995572d2fbdb
Hugo Chavar <hechavar@gmail.com>
2012-06-05 10:37:06 -0300

    Bombita y los Lopez Reggae Alado se mueven correctamente Bombita no Atravieza obstaculos y los Lopez Reggae Alado si atraviezan. Llegue a las 95 pruebas

--- Source/TestBomberman/TestJuego/TestJuego.cs
+++ Source/TestBomberman/TestJuego/TestJuego.cs
@@ -87,9 +87,10 @@ namespace TestBomberman.TestJuego
         }
 
         [Test]
-        public void BombitaCambiaDePosicionEnUnaUnidadDentroDelMapaAlMoversePorPasillo()
+        public void BombitaConVelocidadNormalCambiaDePosicionEnUnaUnidadDentroDelMapaAlMoversePorPasillo()
         {
             this.movil = new Bombita(new Punto(0, 0));
+            Punto posOriginal = this.movil.Posicion.Clonar();
             this.unMapa.AgregarPersonaje(this.movil); // testear todos los exceptions
             this.movil.Movimiento.CambiarADerecha();
             this.movil.Mover();
@@ -97,20 +98,129 @@ namespace TestBomberman.TestJuego
             Assert.IsTrue(pos.Equals(this.movil.Posicion));
         }
 
+        [Test]
+        public void UnMovilDespuesDeMoverseEnDireccionAUnPasilloSuPosicionNoEsIgualALaOriginal()
+        {
+            IMovible otroMovil = new Bombita(new Punto(0, 0));
+            Punto posOriginal = otroMovil.Posicion.Clonar();
+            this.unMapa.AgregarPersonaje(otroMovil); // testear todos los exceptions
+            otroMovil.Movimiento.CambiarADerecha();
+            otroMovil.Mover();
+            //Punto pos = new Punto(1, 0);
+            Assert.IsFalse(posOriginal.Equals(otroMovil.Posicion));
+        }
+
+        [Test]
+        public void UnMovilDespuesDeMoverseEnDireccionAUnObstaculoSuPosicionEsIgualALaOriginal()
+        {
+            IMovible otroMovil = new Bombita(new Punto(1, 0));
+            Punto posOriginal = otroMovil.Posicion.Clonar();
+            this.unMapa.AgregarPersonaje(otroMovil); // testear todos los exceptions
+            otroMovil.Movimiento.CambiarAArriba();
+            otroMovil.Mover();
+            Assert.IsTrue(posOriginal.Equals(otroMovil.Posicion));
+        }
 
         [Test]
         public void BombitaChocaConObstaculoMoverNoCambiaSuPosicion()
         {
-            this.movil = new Bombita(new Punto(0, 0));
-            this.unMapa.AgregarPersonaje(this.movil); // testear todos los exceptions
-            this.movil.Movimiento.CambiarADerecha();
-            this.movil.Mover();
-            this.movil.Movimiento.CambiarAIzquierda();
-            this.movil.Mover();
-            Punto posOriginal = this.movil.Posicion.Clonar();
-            Punto posFrenteSuyo = this.movil.Posicion.PosicionSuperior();
-            Punto posTest = new Punto(1, 1);
-            Assert.IsTrue(posTest.Equals(posFrenteSuyo));
+            IMovible otroMovil = new Bombita(new Punto(0, 0));
+            this.unMapa.AgregarPersonaje(otroMovil); // testear todos los exceptions
+            //Punto posOriginal = otroMovil.Posicion.Clonar();
+            otroMovil.Movimiento.CambiarADerecha();
+            otroMovil.Mover();
+            otroMovil.Movimiento.CambiarAArriba();
+            otroMovil.Mover();
+            Punto posObstaculo = new Punto(1, 1);
+            Assert.IsFalse(otroMovil.Posicion.Equals(posObstaculo));
         }
+
+        [Test]
+        public void BombitaAvanzaPorTodoElMapaYNoCambiaDePosCuandoChocaConElLimiteDerechoDelMapa()
+        {
+            IMovible otroMovil = new Bombita(new Punto(0, 0));
+            this.unMapa.AgregarPersonaje(otroMovil); // testear todos los exceptions
+            //Punto posOriginal = otroMovil.Posicion.Clonar();
+            otroMovil.Movimiento.CambiarAArriba();
+            otroMovil.Mover();//fue a 0,1
+            otroMovil.Mover();//fue a 0,2
+            otroMovil.Movimiento.CambiarADerecha();
+            otroMovil.Mover(); //fue a 1,2
+            otroMovil.Mover();//fue a 2,2
+            otroMovil.Movimiento.CambiarAArriba();
+            otroMovil.Mover(); //fue a 2,3
+            otroMovil.Mover();//fue a 2,4
+            otroMovil.Movimiento.CambiarADerecha();
+            otroMovil.Mover(); //fue a 3,4
+            otroMovil.Mover();//fue a 4,4, esta en el limite!
+            otroMovil.Mover(); //choca con el limite
+            Punto posFinal = new Punto(4, 4);
+            Assert.IsTrue(otroMovil.Posicion.Equals(posFinal));
+        }
+
+        [Test]
+        public void BombitaNoCambiaDePosCuandoChocaConElLimiteIzquierdoDelMapa()
+        {
+            IMovible otroMovil = new Bombita(new Punto(0, 2));
+            Punto posOriginal = otroMovil.Posicion.Clonar();
+            this.unMapa.AgregarPersonaje(otroMovil); // testear todos los exceptions
+            otroMovil.Movimiento.CambiarAIzquierda();
+            otroMovil.Mover(); //choca con el limite izquierdo
+            Assert.IsTrue(otroMovil.Posicion.Equals(posOriginal));
+        }
+
+        [Test]
+        public void BombitaNoCambiaDePosCuandoChocaConElLimiteInferiorDelMapa()
+        {
+            IMovible otroMovil = new Bombita(new Punto(3, 0));
+            Punto posOriginal = otroMovil.Posicion.Clonar();
+            this.unMapa.AgregarPersonaje(otroMovil); // testear todos los exceptions
+            otroMovil.Movimiento.CambiarAAbajo();
+            otroMovil.Mover(); //choca con el limite inferior
+            Assert.IsTrue(otroMovil.Posicion.Equals(posOriginal));
+        }
+
+        [Test]
+        public void BombitaNoCambiaDePosCuandoChocaConElLimiteSuperiorDelMapa()
+        {
+            IMovible otroMovil = new Bombita(new Punto(2, 4));
+            Punto posOriginal = otroMovil.Posicion.Clonar();
+            this.unMapa.AgregarPersonaje(otroMovil); // testear todos los exceptions
+            otroMovil.Movimiento.CambiarAArriba();
+            otroMovil.Mover(); //choca con el limite superior
+            Assert.IsTrue(otroMovil.Posicion.Equals(posOriginal));
+        }
+
+        [Test]
+        public void UnLopezReggaeAladoAvanzaSobreUnObstaculoSuPosicionEsDistintaALaOriginal()
+        {
+            IMovible otroMovil = new LosLopezReggaeAlado(new Punto(1, 0));
+            Punto posOriginal = otroMovil.Posicion.Clonar();
+            this.unMapa.AgregarPersonaje(otroMovil); // testear todos los exceptions
+            otroMovil.Movimiento.CambiarAArriba();
+            otroMovil.Mover(); //se mueve donde hay un bloque de acero
+            Assert.IsFalse(otroMovil.Posicion.Equals(posOriginal));
+        }
+
+        [Test]
+        public void UnLopezReggaeAladoAtraviezaObstaculos()
+        {
+            IMovible otroMovil = new LosLopezReggaeAlado(new Punto(0, 0));
+            this.unMapa.AgregarPersonaje(otroMovil); // testear todos los exceptions
+            Punto posOriginal = otroMovil.Posicion.Clonar();
+            otroMovil.Movimiento.CambiarAArriba();
+            otroMovil.Mover();//fue a 0,1
+            otroMovil.Movimiento.CambiarADerecha();
+            otroMovil.Mover();//fue a 1,1 (esta sobre un bloque de acero)
+            otroMovil.Mover(); //fue a 2,1 (esta sobre pasillo)
+            otroMovil.Mover();//fue a 3,1 (esta sobre un bloque de acero)
+            otroMovil.Movimiento.CambiarAArriba();
+            otroMovil.Mover(); //fue a 3,2 (esta sobre pasillo)
+            otroMovil.Mover();//fue a 3,3 (esta sobre un bloque de acero)
+            otroMovil.Mover(); //fue a 3,4
+            Punto posFinal = new Punto(3, 4);
+            Assert.IsTrue(otroMovil.Posicion.Equals(posFinal));
+        }
+
     }
 }
