4c09412c6cccee2407fe17ff40fdcae53263930d
Mart?n Carrera <martinc.92@hotmail.com>
2012-06-07 10:08:12 -0300

    Agrego algunas cosas para terminar de cerrar el tema de la salida. No me terminan de cerrar algunas cosas.

--- Source/Bomberman/Mapa/Casilla/Casilla.cs
+++ Source/Bomberman/Mapa/Casilla/Casilla.cs
@@ -77,11 +77,20 @@ namespace Bomberman.Mapa.Casilla
 
         public void agregarArticulo(Articulo.Articulo unArticulo)
         {
-            if ((this.estado == null) | (this.estado.PuedeAgregarArticulo()))   
+            if ((this.Estado != null) & (this.estado.PuedeAgregarArticulo()))   
             {
             this.ArticuloContenido = unArticulo;
             }
 
+        }
+
+        public void agregarSalida()
+        { 
+            if ((this.Estado != null) & (this.estado.PuedeContenerSalida()) & (Juego.Juego.Instancia().Ambiente.PosicionSalida == null))
+            {
+                this.ArticuloContenido = new Articulo.Salida();
+                Juego.Juego.Instancia().Ambiente.PosicionSalida = this.Posicion;
+            }
         }
 
         //public abstract void DaniarConBombaToleTole();
