c5674e2005186d69b18403c8800424d48fabe933
Hugo Chavar <hechavar@gmail.com>
2012-06-07 21:52:52 -0300

    Peque?os test de precalientamiento

--- Source/TestBomberman/TestPunto.cs
+++ Source/TestBomberman/TestPunto.cs
@@ -32,6 +32,22 @@ namespace TestBomberman
         }
 
         [Test]
+        public void EqualsDevuelveFalseSiElPuntoEsComparadoConOtroObjetoQueNoEsUnPunto()
+        {
+            string algo = "prueba Equals de Punto";
+
+            Assert.IsFalse(this.posicion.Equals(algo));
+        }
+
+        [Test]
+        public void GetHashCodeDevuelveElMismoNumeroParaDosPuntoIguales()
+        {
+            Punto pa = new Punto(3, 4);
+            Punto pb = new Punto(3, 4);
+            Assert.AreEqual(pa.GetHashCode(),pb.GetHashCode());
+        }
+
+        [Test]
         public void ClonarDevuelveUnNuevoPuntoConCordenadasIgualesAlPuntoClonado()
         {
             Punto otroPunto = this.posicion.Clonar();
@@ -66,7 +82,6 @@ namespace TestBomberman
         [Test]
         public void PosicionInferriorDevuelveUnaNuevaPosicionConCoordenadaYDisminuidaEn1()
         {
-            //Punto posicionInferior = this.posicion.PosicionInferior();
             Punto p = new Punto(3, 4);
             Punto posicionInferior = p.PosicionInferior();
             Assert.IsTrue(posicionInferior.Equals(new Punto(3, 3)));
@@ -83,7 +98,6 @@ namespace TestBomberman
         [Test]
         public void PosicionIzquierdaDevuelveUnaNuevaPosicionConCoordenadaXDisminuidaEn1()
         {
-            
             Punto p = new Punto(3, 4);
             Punto posicionIzquierda = p.PosicionIzquierda();
             Assert.IsTrue(posicionIzquierda.Equals(new Punto(2, 4)));
