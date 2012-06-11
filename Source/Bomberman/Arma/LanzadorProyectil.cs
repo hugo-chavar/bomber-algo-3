b353a8cb7cf4f59d2aeff027043c49105499dc15
Ezequiel <ezemr_cuervo@hotmail.com>
2012-06-11 02:42:22 -0300

    Queda 1, vamos tincho que tenes el mas jodido, ponga huevo y vaya al frente ! jaja

---
+++ Source/Bomberman/Arma/LanzadorProyectil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bomberman.Personaje;
+using Bomberman.Mapa;
+
+namespace Bomberman.Arma
+{
+    public class LanzadorProyectil : Lanzador
+    {
+
+        private const int ALCANCELANZAMIENTO = 3;
+
+        public LanzadorProyectil()
+        {
+            this.Alcance = ALCANCELANZAMIENTO;            
+        }
+
+        public override void Cargar(IMovible movil)
+        {
+            base.Cargar(movil);
+            this.carga = new Proyectil(this.posicionDeTiro);
+        }
+
+        public override void Disparar()
+        {
+            Juego.Juego.Instancia().ObjetoContundenteLanzado(this.carga);
+        }
+    }
+}
---
+++ Source/Bomberman/Mapa/Casilla/BloqueAcero.cs
@@ -0,0 +1,22 @@
+﻿using Bomberman;
+namespace Bomberman.Mapa.Casilla
+{
+    public class BloqueAcero : Obstaculo
+    {
+        private const int RESISTENCIAACERO = 10;
+        public BloqueAcero()
+            : base(RESISTENCIAACERO)
+        { }
+
+        public override void DaniarConBombaMolotov(int UnidadesDaniadas)
+        {           
+        }
+
+        public override bool PuedeContenerSalida()
+        {
+            return false;
+        }
+
+        //public abstract void daniarConProyectil();
+     }
+}
