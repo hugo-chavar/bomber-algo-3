1a725a82f9d69947534313076839711f0217c612
Mart?n Carrera <martinc.92@hotmail.com>
2012-06-11 01:40:26 -0300

    Acomodo tests de salida! Quedan 5!!!

---
+++ Source/Bomberman/Articulo/Salida.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bomberman.Articulo
+{
+    public class Salida : Articulo
+    {
+        public Salida()
+        {
+            this.estaActivo = false;
+        }
+
+        public override void ModificarComedor(Personaje.IComedor comedor)
+        {
+            if (this.EstaActivo)
+            {
+                comedor.FinalizarNivel();
+            }
+        }
+    }
+}
---
+++ Source/Bomberman/Personaje/IComedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bomberman.Articulo;
+
+namespace Bomberman.Personaje
+{
+    public interface IComedor
+    {
+        void Comer(IComible comible);
+
+        void DuplicarVelocidad();
+        void CambiarLanzadorAToleTole();
+        void ReducirRetardo(int retardo);
+
+        void FinalizarNivel();
+    }
+}
