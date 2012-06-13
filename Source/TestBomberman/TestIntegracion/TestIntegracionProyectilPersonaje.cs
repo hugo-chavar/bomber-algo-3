67cb7fc1d31cff199d8dea37b670127f2e2ee481
Mart?n Carrera <martinc.92@hotmail.com>
2012-06-13 11:44:25 -0300

    Subo los primeros 4 tests que hay q hacer!!

--- Source/TestBomberman/TestIntegracion/TestIntegracionProyectilPersonaje.cs
+++ Source/TestBomberman/TestIntegracion/TestIntegracionProyectilPersonaje.cs
@@ -5,6 +5,9 @@ using System.Text;
 using Bomberman.Juego;
 using Bomberman.Mapa;
 using NUnit.Framework;
+using Bomberman.Personaje;
+using Bomberman;
+using Bomberman.Mapa.Casilla;
 
 namespace TestBomberman.TestIntegracion
 {
@@ -27,5 +30,76 @@ namespace TestBomberman.TestIntegracion
             Juego.Reiniciar();
         }
 
+        [Test]
+        public void TiroUnProyectilATresCasillasDeDistanciaYLosObjetosGolpeadosBajanSuResistenciaEnUnaUnidad()
+        {
+            Personaje unTipoQueTiraProyectiles = new LosLopezReggae(new Punto(13,6));
+
+            unTipoQueTiraProyectiles.Movimiento.CambiarADerecha();
+            unTipoQueTiraProyectiles.LanzarExplosivo();
+            int resistenciaLadrillo1 = Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 5)).Estado.UnidadesDeResistencia;
+            int resistenciaLadrillo2 = Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 7)).Estado.UnidadesDeResistencia;
+
+            Juego.Instancia().AvanzarElTiempo();
+            Juego.Instancia().AvanzarElTiempo();
+            Juego.Instancia().AvanzarElTiempo();
+
+            Assert.AreEqual(resistenciaLadrillo1-1,Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 5)).Estado.UnidadesDeResistencia);
+            Assert.AreEqual(resistenciaLadrillo2-1,Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 7)).Estado.UnidadesDeResistencia);
+        }
+
+        [Test]
+        public void TiroUnProyectilADosCasillasDeDistanciaYLosObjetosGolpeadosBajanSuResistenciaEnUnaUnidad()
+        {
+            Personaje unTipoQueTiraProyectiles = new LosLopezReggae(new Punto(14, 6));
+
+            unTipoQueTiraProyectiles.Movimiento.CambiarADerecha();
+            unTipoQueTiraProyectiles.LanzarExplosivo();
+            int resistenciaLadrillo1 = Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 5)).Estado.UnidadesDeResistencia;
+            int resistenciaLadrillo2 = Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 7)).Estado.UnidadesDeResistencia;
+
+            Juego.Instancia().AvanzarElTiempo();
+            Juego.Instancia().AvanzarElTiempo();
+            Juego.Instancia().AvanzarElTiempo();
+            
+            Assert.AreEqual(resistenciaLadrillo1 - 1, Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 5)).Estado.UnidadesDeResistencia);
+            Assert.AreEqual(resistenciaLadrillo2 - 1, Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 7)).Estado.UnidadesDeResistencia);
+        }
+
+        [Test]
+        public void TiroUnProyectilAUnaCasillasDeDistanciaYLosObjetosGolpeadosBajanSuResistenciaEnUnaUnidad()
+        {
+            Personaje unTipoQueTiraProyectiles = new LosLopezReggae(new Punto(15, 6));
+
+            unTipoQueTiraProyectiles.Movimiento.CambiarADerecha();
+            unTipoQueTiraProyectiles.LanzarExplosivo();
+            int resistenciaLadrillo1 = Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 5)).Estado.UnidadesDeResistencia;
+            int resistenciaLadrillo2 = Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 7)).Estado.UnidadesDeResistencia;
+
+            Juego.Instancia().AvanzarElTiempo();
+            Juego.Instancia().AvanzarElTiempo();
+            
+            Assert.AreEqual(resistenciaLadrillo1 - 1, Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 5)).Estado.UnidadesDeResistencia);
+            Assert.AreEqual(resistenciaLadrillo2 - 1, Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(16, 7)).Estado.UnidadesDeResistencia);
+        }
+
+        [Test]
+        public void TiroCincoProyectilesAUnLadrilloYElMismoSeDestruye()
+        {
+            Personaje unTipoQueTiraProyectiles = new LosLopezReggae(new Punto(11, 12));
+
+            unTipoQueTiraProyectiles.Movimiento.CambiarADerecha();
+            unTipoQueTiraProyectiles.LanzarExplosivo();
+            unTipoQueTiraProyectiles.LanzarExplosivo();
+            unTipoQueTiraProyectiles.LanzarExplosivo();
+            unTipoQueTiraProyectiles.LanzarExplosivo();
+            unTipoQueTiraProyectiles.LanzarExplosivo();
+
+            Juego.Instancia().AvanzarElTiempo();
+            Juego.Instancia().AvanzarElTiempo();
+            Juego.Instancia().AvanzarElTiempo();
+
+            Assert.IsInstanceOf(typeof(Pasillo), Juego.Instancia().Ambiente.ObtenerCasilla(new Punto(14, 12)).Estado);
+        }
     }
 }
