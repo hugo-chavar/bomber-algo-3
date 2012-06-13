5e385e639f7d7902412c74c64a7fced4cd50de4f
Mart?n Carrera <martinc.92@hotmail.com>
2012-06-13 11:27:34 -0300

    Agrego una clase donde probar el proyectil con el personaje, para hacer los tests q mando hugo Laburen en esta clase!

---
+++ Source/TestBomberman/TestIntegracion/TestIntegracionProyectilPersonaje.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bomberman.Juego;
+using Bomberman.Mapa;
+using NUnit.Framework;
+
+namespace TestBomberman.TestIntegracion
+{
+    class TestIntegracionProyectilPersonaje
+    {
+        private Juego unJuego;
+        private Mapa unMapa;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            this.unJuego = Juego.Instancia();
+            this.unMapa = this.unJuego.Ambiente;
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Juego.Reiniciar();
+        }
+
+    }
+}
