cb8d59d69ceeb65786cfe039d512986f7dffe5a2
Hugo Chavar <hechavar@gmail.com>
2012-06-23 20:39:29 -0300

    Agrego la estructura para XNA, agrego los dibujitos de los sprites, agrego clase cargador de mapa. agrego archivo xml conteniendo la definicion del mapa incompleto.. Borro cosas innecesarias.

---
+++ Source/Bomberman/Juego/CargadorDeMapa.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using BombermanModel.Mapa.Casilla;
+
+namespace BombermanModel.Juego
+{
+    class CargadorDeMapa
+    {
+        public bool LeerMapa(string pathName)
+        {
+            try
+            {
+                StreamReader lector = new StreamReader(pathName);
+
+                //asigno el texto xml a una variable
+                string xmlTexto = lector.ReadToEnd();
+
+                //separar todos los objetos
+                string[] todosLosObjetos = SepararObjetos(xmlTexto);
+
+                //crear los objetos a partir del array
+                CrearLosObjetos(todosLosObjetos);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+        public void CrearLosObjetos(string[] arrayString)
+        {
+            foreach (string s in arrayString)
+            {
+                CrearObjeto(s);
+            }
+
+        }
+
+        public void CrearObjeto(string s)
+        {
+            string clase = ObtenerAtributo(s,"class");
+            int x = Convert.ToInt32(ObtenerAtributo(s, "x")); 
+            int y = Convert.ToInt32(ObtenerAtributo(s, "y"));
+
+            Casilla unaCasilla;
+            switch (clase)
+            {
+                case "BloqueLadrillo":
+                    unaCasilla = FabricaDeCasillas.FabricarCasillaConBloqueLadrillos(new Punto(x,y));
+                    Juego.Instancia().Ambiente.AgregarCasilla(unaCasilla);
+                    break;
+                case "BloqueCemento":
+                    unaCasilla = FabricaDeCasillas.FabricarCasillaConBloqueCemento(new Punto(x, y));
+                    Juego.Instancia().Ambiente.AgregarCasilla(unaCasilla);
+                    break;
+                case "Pasillo":
+                    unaCasilla = FabricaDeCasillas.FabricarCasillaConBloqueLadrillos(new Punto(x, y));
+                    Juego.Instancia().Ambiente.AgregarCasilla(unaCasilla);
+                    break;
+                case "BloqueAcero":
+                    unaCasilla = FabricaDeCasillas.FabricarCasillaConBloqueCemento(new Punto(x, y));
+                    Juego.Instancia().Ambiente.AgregarCasilla(unaCasilla);
+                    break;
+            }
+
+        }
+        public string ObtenerAtributo(string stringObjeto, string stringAtributo)
+        {
+            int pos = stringObjeto.IndexOf(stringAtributo+"\"");
+            if (pos != -1)
+            {
+                int inicio = pos;
+                int fin = stringObjeto.IndexOf("\"", pos + 1);
+                int len = fin + 1 - inicio;
+                return stringObjeto.Substring(inicio, len);
+            }
+            else
+            {
+                return "";
+            }
+        }
+        public string[] SepararObjetos(string xmlTexto)
+        {
+            List<string> listaString = new List<string>();
+            //Eliminar cosas innecesarias
+            xmlTexto = xmlTexto.Replace("\t", "");
+            xmlTexto = xmlTexto.Replace("\n", "");
+            xmlTexto = xmlTexto.Replace("<Objects>", "");
+            xmlTexto = xmlTexto.Replace("</Objects>", "");
+
+            string inicioObjeto = "<obj ";
+            int pos = xmlTexto.IndexOf(inicioObjeto);
+            if (pos != -1)
+            {
+                xmlTexto = xmlTexto.Remove(pos,inicioObjeto.Length);
+            }
+
+            while (pos != -1)
+            {
+                int inicio = pos;
+                int fin = xmlTexto.IndexOf("/>");
+                int len = fin + 1 - inicio;
+                listaString.Add(xmlTexto.Substring(inicio, len));
+
+
+                pos = xmlTexto.IndexOf(inicioObjeto,pos+1);
+            }
+            return listaString.ToArray();
+        }
+    }
+}
