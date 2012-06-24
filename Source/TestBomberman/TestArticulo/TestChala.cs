7020d0ebffe471de3c0b02331f5b91c3d163b711
Andy Sanabria <andresg.sanabria@gmail.com>
2012-06-23 22:07:12 -0300

    Agrego el cargar articulos y enemigos al cargadordeMapa

---
+++ Source/TestBomberman/TestArticulo/TestChala.cs
@@ -0,0 +1,34 @@
+using BombermanModel.Articulo;
+using BombermanModel.Personaje;
+using NUnit.Framework;
+using BombermanModel;
+using BombermanModel.Mapa.Casilla;
+
+
+namespace TestBombermanModel.TestArticulo
+{   [TestFixture]
+    class TestChala
+    {
+        private Punto pos;
+        private Casilla c;
+        private Bombita unBombita;
+    
+        [SetUp]
+        public void TestSetup()
+        {
+            pos = new Punto(3, 4);
+            c = FabricaDeCasillas.FabricarPasillo(pos);
+            unBombita = new Bombita(pos);
+        }
+    
+        [Test]
+        public void ComerChalaDuplicaVelocidadDelComedor()
+        {
+            int velocidad = unBombita.Movimiento.Velocidad;
+            Articulo unArticulo = new Chala();
+            unBombita.Comer(unArticulo);
+
+            Assert.AreEqual(2*velocidad, unBombita.Movimiento.Velocidad);
+         }
+    }
+}
