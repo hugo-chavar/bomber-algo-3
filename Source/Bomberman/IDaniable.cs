8b28c09ba44bd210c2c3afb8d74319f36f44ecdf
Hugo Chavar <hechavar@gmail.com>
2012-06-24 22:00:35 -0300

    Saco cosas harcodeadas

---
+++ Source/Bomberman/IDaniable.cs
@@ -0,0 +1,12 @@
+﻿using BombermanModel.Personaje;
+namespace BombermanModel
+{
+    public interface IDaniable
+    {
+        void DaniarConBombaToleTole();
+        void DaniarConBombaMolotov(int UnidadesDaniadas);
+        void DaniarConProyectil(int UnidadesDaniadas);
+        //void DaniarPorColision(int UnidadesDaniadas);
+        bool Destruido();
+    }
+}
---
+++ Source/Bomberman/Personaje/Cecilio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BombermanModel.Arma;
+
+namespace BombermanModel.Personaje
+{
+    public class Cecilio : Enemigo
+    {
+        private const int VIDACECILIO = 5;
+
+        public Cecilio(Punto unPunto)
+            : base(unPunto)
+        {
+            this.Lanzador = new LanzadorMolotov();
+            this.UnidadesDeResistencia = VIDACECILIO;
+        }
+
+    }
+}
