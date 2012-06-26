22d19f3ffef941db1bc89649cb529678053940d7
Hugo Chavar <hechavar@gmail.com>
2012-06-26 14:13:18 -0300

    Elimino el metodo Colision

---
+++ Source/Bomberman/Personaje/LosLopezReggaeAlado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BombermanModel.Arma;
+
+namespace BombermanModel.Personaje
+{
+    public class LosLopezReggaeAlado : Enemigo
+    {
+        private const int VIDALOSLOPEZREGGAEALADO = 5;
+
+        public LosLopezReggaeAlado(Punto unPunto)
+            : base(unPunto)
+        {
+            this.Lanzador = new LanzadorMolotov();
+            this.UnidadesDeResistencia = VIDALOSLOPEZREGGAEALADO;
+            this.Movimiento.Velocidad = 1;
+        }
+
+        public override bool AtraviesaObstaculos()
+        {
+            return true;
+        }
+    }
+}
