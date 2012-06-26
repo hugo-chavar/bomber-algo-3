308c90db41439283c66d2582ee4f96c9cbf31527
Hugo Chavar <hechavar@gmail.com>
2012-06-26 01:16:29 -0300

    LRA terminado.. habria que probar matarlo.

--- Source/WindowsGame1/WindowsGame1/LopezReggaeAladoView.cs
+++ Source/WindowsGame1/WindowsGame1/LopezReggaeAladoView.cs
@@ -21,11 +21,16 @@ namespace BombermanGame
     {
 
         private Personaje unLRA = new LosLopezReggaeAlado(new Punto(10,10));
+        private Vector2 puntoCentro;
+        private int destinoActual =  0;
+        private List<Point> destinosObjetivo = new List<Point>();
+
         public LopezReggaeAladoView(Vector2 pos)
             : base(pos)
         {
             speed = 0.3f;// unLRA.Movimiento.Velocidad;
-            spriteName = "LRAlado";
+            spriteName = "LRA";
+            
         }
 
 
@@ -37,28 +42,63 @@ namespace BombermanGame
         //        res += 360;
         //    return res;
         //}
+        private void CargarObjetivos()
+        {
+            Random direccionRandom = new Random();
+            //ingreso una lista al azar de 10 objetivos del LRA
+            for (int i = 0; i < 10; i++)
+            {
+                int x = direccionRandom.Next(Juego.Instancia().Ambiente.DimensionHorizontal);
+                int y = direccionRandom.Next(Juego.Instancia().Ambiente.DimensionVertical);
+                destinosObjetivo.Add(new Point(x, y));
+            }
+        }
+
+        private void ProximoDestino()
+        {
+            //si fue a todos los destinos empieza de nuevo
+            if (destinoActual == destinosObjetivo.Count - 1) destinoActual = 0;
+            else destinoActual++;
+        }
+
+
         public override void LoadContent(ContentManager content)
         {
             position.X = 32*unLRA.Posicion.X + Game1.mapa.Location.X;
             position.Y = 32*unLRA.Posicion.Y + Game1.mapa.Location.Y;
             spriteIndex = content.Load<Texture2D>("Sprites\\" + spriteName);
             area = new Rectangle(0, 0, spriteIndex.Width, spriteIndex.Height);
+            puntoCentro = new Vector2(spriteIndex.Width / 2, spriteIndex.Height / 2);
+            CargarObjetivos();
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            Vector2 center = new Vector2(spriteIndex.Width / 2, spriteIndex.Height / 2);
-  
-            spriteBatch.Draw(spriteIndex, position, null, Color.White, MathHelper.ToRadians(rotation), center, scale, SpriteEffects.None, 0);
+            
+
+            spriteBatch.Draw(spriteIndex, position, null, Color.White, MathHelper.ToRadians(rotation), puntoCentro, scale, SpriteEffects.None, 0);
             //spriteBatch.DrawString(Game1.fuente, "En modelo ->Pos X: " + unLRA.Posicion.X + " Pos Y: " + unLRA.Posicion.Y, new Vector2(10, 10), Color.Yellow);
         }
 
         public override void Update()
         {
-            rotation = point_direction(position.X, position.Y, Juego.Instancia().Protagonista.Posicion.X * 32 + Game1.mapa.Location.X, Juego.Instancia().Protagonista.Posicion.Y * 32 + Game1.mapa.Location.Y);
+
+            rotation = point_direction(position.X, position.Y, destinosObjetivo[destinoActual].X * 32 + Game1.mapa.Location.X, destinosObjetivo[destinoActual].Y * 32 + Game1.mapa.Location.Y);
             if (!vivo) return;
             PushTo(speed, rotation);
+            ActualizarPosicion();
             base.Update();
+        }
+
+        public void ActualizarPosicion()
+        {
+            int x = ((int)Math.Round(position.X,0) - Game1.mapa.Location.X) / 32;
+            int y = ((int)Math.Round(position.Y, 0) - Game1.mapa.Location.Y) / 32;
+            Juego.Instancia().Ambiente.ObtenerCasilla(unLRA.Posicion).Dejar(unLRA);
+            unLRA.Posicion = new Punto(x, y);
+            Juego.Instancia().Ambiente.ObtenerCasilla(unLRA.Posicion).Transitar(unLRA);
+            if ((destinosObjetivo[destinoActual].X == x) && (destinosObjetivo[destinoActual].Y == y))
+                ProximoDestino();
         }
 
         public float point_direction(float x, float y, float x2, float y2)
@@ -79,8 +119,8 @@ namespace BombermanGame
         {
             float newX = (float)Math.Cos(MathHelper.ToRadians(dir));
             float newY = (float)Math.Sin(MathHelper.ToRadians(dir));
-            position.X += pix * (float)newX; //
-            position.Y += pix * (float)newY; //pix *
+            position.X += pix * (float)newX;
+            position.Y += pix * (float)newY; 
         }
 
                   
